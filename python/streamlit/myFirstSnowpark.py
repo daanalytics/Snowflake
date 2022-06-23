db8c1afcf529be1ce7c6fc781512bca088aaeacf
Daan Bakboord <cloud@daanalytics.nl>
2022-06-24 00:18:44 +0200

    Changing credential location

--- python/streamlit/myFirstSnowpark.py
+++ python/streamlit/myFirstSnowpark.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 
 workdir = os.getcwd()
-conndir = workdir + '/_streamlit/'
+conndir = workdir + '/.streamlit/'
 connfile = conndir + 'cred.json'
 
 # Create Session object
