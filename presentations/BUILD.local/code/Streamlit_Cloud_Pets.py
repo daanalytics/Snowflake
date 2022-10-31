679cc8f1c812d32e3e65f063aded2a0f909327b2
Daan Bakboord <dbakboord@daanalytics.nl>
2022-10-31 18:34:39 +0100

    Create Streamlit_Cloud_Pets.py

---
+++ presentations/BUILD.local/code/Streamlit_Cloud_Pets.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import snowflake.connector
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Connect to DEMO_DB database
+conn.cursor().execute("USE DATABASE demo_db")
+
+# Connect to PETS schema
+conn.cursor().execute("USE SCHEMA pets")
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * FROM pets;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
