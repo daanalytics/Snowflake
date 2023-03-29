94386d8c449bb9d40357ad57d787c63e35c2ea92
Daan Bakboord <cloud@daanalytics.nl>
2023-03-29 09:33:16 +0200

    Changed Query parsing

--- python/streamlit/quality_assurance.py
+++ python/streamlit/quality_assurance.py
@@ -59,15 +59,22 @@ if __name__ == "__main__":
         # Where clause for the Fully Qualified Table Name
         wc_full_qual_table_name = database_name + '.' + schema_name + '.' + table_name
 
-        sql_quality_metrics = pd.read_sql("""SELECT *
-                                        FROM   QUALITY_ASSURANCE.QUALITY_CHECK.DATA_QUALITY_METRICS
-                                        WHERE  FULL_QUAL_TABLE_NAME = {wc_full_qual_table_name} """, ctx)
+        #sql_quality_metrics = pd.read_sql("""SELECT *
+        #                                FROM   QUALITY_ASSURANCE.QUALITY_CHECK.DATA_QUALITY_METRICS
+        #                                WHERE  FULL_QUAL_TABLE_NAME = {wc_full_qual_table_name} """, ctx)
+        
+        sql_quality_metrics = """SELECT *
+                                FROM   QUALITY_ASSURANCE.QUALITY_CHECK.DATA_QUALITY_METRICS
+                                WHERE  FULL_QUAL_TABLE_NAME = %wc_full_qual_table_name
+                                ; """
      
         st.write("Data Quality Metrics for table: " + table_name)
 
-        df_quality_metrics = pd.DataFrame(sql_quality_metrics, columns=['DATABASE_NAME', 'SCHEMA_NAME', 'TABLE_NAME', 'FULL_QUAL_TABLE_NAME', 'COLUMN_NAME', 'TOTAL_COUNT', 'NOT_NULL_COUNT', 'NULL_COUNT', 'BLANK_COUNT', 'DISTINCT_VALUES_COUNT'
-        , 'MAX_LENGTH', 'MIN_LENGTH', 'MAX_VALUE', 'MIN_VALUE', 'NUMERIC_ONLY_VALUES_COUNT', 'ALPHABETS_ONLY_VALUES_COUNT', 'ALPHANUMERIC_ONLY_VALUES_COUNT'
-        , 'CONTAINS_SPECIAL_CHAR_COUNT', 'TOP_TEN_DISTINCT_VALUES'])
+        df_quality_metrics = pd.read_sql_query(sql_quality_metrics, ctx, params={wc_full_qual_table_name})
+
+        #df_quality_metrics = pd.DataFrame(sql_quality_metrics, columns=['DATABASE_NAME', 'SCHEMA_NAME', 'TABLE_NAME', 'FULL_QUAL_TABLE_NAME', 'COLUMN_NAME', 'TOTAL_COUNT', 'NOT_NULL_COUNT', 'NULL_COUNT', 'BLANK_COUNT', 'DISTINCT_VALUES_COUNT'
+        #, 'MAX_LENGTH', 'MIN_LENGTH', 'MAX_VALUE', 'MIN_VALUE', 'NUMERIC_ONLY_VALUES_COUNT', 'ALPHABETS_ONLY_VALUES_COUNT', 'ALPHANUMERIC_ONLY_VALUES_COUNT'
+        #, 'CONTAINS_SPECIAL_CHAR_COUNT', 'TOP_TEN_DISTINCT_VALUES'])
 
         st.table(df_quality_metrics)
 
